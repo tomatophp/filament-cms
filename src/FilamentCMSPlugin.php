c535ac9a6acf6a269254842ff13ce697bcf5431a
Fady Mondy <info@3x1.io>
2024-09-17 19:37:05 +0300

    fix ->isEnabled()

--- src/FilamentCMSPlugin.php
+++ src/FilamentCMSPlugin.php
@@ -39,10 +39,8 @@ class FilamentCMSPlugin implements Plugin
 
     public function register(Panel $panel): void
     {
-        if(class_exists(Module::class)){
-            if(\Nwidart\Modules\Facades\Module::find('FilamentCms')?->isEnabled()){
-                $this->isActive = true;
-            }
+        if(class_exists(Module::class) && \Nwidart\Modules\Facades\Module::find('FilamentCms')?->isEnabled()){
+            $this->isActive = true;
         }
         else {
             $this->isActive = true;
